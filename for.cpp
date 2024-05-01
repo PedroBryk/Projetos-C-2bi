1d1d7fa23adaea07caaaedf3c0665e3235fd5d60
Pedro Bryk <88406806+PedroBryk@users.noreply.github.com>
2024-04-30 21:18:55 -0300

    Initial commit

---
+++ for.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    cout<<"Contador de 1 a 10"<<endl;
+
+    for ( int i = 1; i <= 10; i++)
+    {
+        cout<<i;
+
+    }
+    
+    return 0;
+}
