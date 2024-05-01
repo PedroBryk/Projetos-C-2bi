852eb8694f7397160aecf1d6d15d8c1f749f2530
Pedro Bryk <88406806+PedroBryk@users.noreply.github.com>
2024-04-30 21:29:58 -0300

    tabuada

---
+++ tabuada.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int tab = 3;
+    int tamanho = 10;
+    int i;
+
+    for ( i = 0 ; i <= tamanho; i++)
+    {
+        cout<<tab<<" X "<<i<<" : "<< tab*i<<endl;
+
+    }
+
+    
+    return 0;
+    
+}
