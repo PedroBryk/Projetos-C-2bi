8dcb9ef96c77d2eef91a1bd67672806719e1137e
Pedro Bryk <88406806+PedroBryk@users.noreply.github.com>
2024-05-02 21:49:37 -0300

    atividades

--- media -4.cpp
+++ media -4.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
 
     int idades;
-    int soma;
+    double soma = 0;
 
     cout<<"Insira a quantidade de medias que voce deseja calcular: "<<endl;
     cin>>idades;
@@ -16,16 +16,16 @@ int main(){
     int idade;
 
     cout<<"Insira o valor da idade: ";
+
     cin>>idade;
-    
-    int soma = idade;
+
+    soma += idade;
 
    }
 
-    float media = soma / idades;
+   double media = soma / idades;
 
     cout<<"A media das idades eh: "<<media;
-   
     
     return 0;
 }
